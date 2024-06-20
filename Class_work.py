85e8d1303eaf4dea16f2c1bebfb2abfdcfe30962
ademipeju <ademipeju5@gmail.com>
2024-06-20 12:39:28 +0100

    python loop

---
+++ Class_work.py
@@ -0,0 +1,54 @@
+
+# def double(x):
+#     print(x*2)
+# LGA= ["Ilorin west", "Ekiti", "Oke-ero", "Ifelodun", "Baruteen", "Asa", "Ilorin south", "Kaima", "Saare"]
+# # # #loop thru the array, print all the lg in the even position
+# length= len(LGA)
+# # for i in range(1,length,2):
+# #     print(LG_in_Kwara[i])
+# for i in range(length):
+#     if (i+1)% 2 == 0:
+#         print(LG_in_Kwara[i])
+#with the help of the range function, print the ones at the odd position
+# for i in range(0,length,2):
+#     print(LG_in_Kwara[i])
+
+# for i in range(length):
+#     if (i+2) % 2 == 0: 
+#         print(LG_in_Kwara[i])
+# # for i in range(length):
+# #     if 0 % 2 == 0:
+# #         print(LG_in_Kwara[i])
+# for i in range(length):
+#     print(LG_in_Kwara[i])
+# for i in range(length):
+#     if (i+2) % 2 ==1:
+#         print(LG_in_Kwara[i])
+# 
+# for i in range(length):
+#     print(LG_in_Kwara[i])
+LGAs= ["Ilorin west", "Ekiti", "Oke-ero", "Ifelodun", "Baruteen", "Asa", "Ilorin south", "Kaima", "Saare"]
+# length=len(LGAs)
+
+# vowel= "aeiou"
+# for i in range(length):
+#     if LGAs[i][0].lower() in vowel:
+#          print(LGAs[i])
+
+# for lg in LGAs:
+#      if lg[0] in vowel:
+#          print(lg)
+# #Assignment
+# #go to w3 schools, check everything on list, also, under loop, do the practice, read on functions and classes
+
+# # double(5)
+# # double(100)
+# # double(8)
+# # double
+# numbers=[1,2,3,4]
+# numbers.pop(1)
+#create an emptylist
+modify_LGAS= []
+for i in LGAs:
+    modify_LGAS.append(i + " LG")
+print(modify_LGAS)
