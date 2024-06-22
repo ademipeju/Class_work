1739569875119fa179534fa7e5cd2165703fde90
ademipeju <ademipeju5@gmail.com>
2024-06-22 22:26:14 +0100

    Python_loop_Assignment

--- Class_work.py
+++ Class_work.py
@@ -1,44 +1,41 @@
 
 # def double(x):
 #     print(x*2)
-# LGA= ["Ilorin west", "Ekiti", "Oke-ero", "Ifelodun", "Baruteen", "Asa", "Ilorin south", "Kaima", "Saare"]
-# # # #loop thru the array, print all the lg in the even position
+LGA= ["Ilorin west", "Ekiti", "Oke-ero", "Ifelodun", "Baruteen", "Asa", "Ilorin south", "Kaima", "Saare"]
+# # # # #loop thru the array, print all the lg at the even position
 # length= len(LGA)
-# # for i in range(1,length,2):
-# #     print(LG_in_Kwara[i])
+# # print(length)
+# for i in range(1,length,2):
+#        print(LGA[i])
 # for i in range(length):
 #     if (i+1)% 2 == 0:
-#         print(LG_in_Kwara[i])
+#         print(LGA[i])
 #with the help of the range function, print the ones at the odd position
 # for i in range(0,length,2):
 #     print(LG_in_Kwara[i])
-
 # for i in range(length):
-#     if (i+2) % 2 == 0: 
-#         print(LG_in_Kwara[i])
-# # for i in range(length):
-# #     if 0 % 2 == 0:
-# #         print(LG_in_Kwara[i])
+#     if (i+2) % 2 == 1: 
+#          print(LGA[i])
 # for i in range(length):
-#     print(LG_in_Kwara[i])
+#     print(LGA[i])
 # for i in range(length):
 #     if (i+2) % 2 ==1:
-#         print(LG_in_Kwara[i])
+#         print(LGA[i])
 # 
 # for i in range(length):
-#     print(LG_in_Kwara[i])
+#     print(LGA[i])
 LGAs= ["Ilorin west", "Ekiti", "Oke-ero", "Ifelodun", "Baruteen", "Asa", "Ilorin south", "Kaima", "Saare"]
 # length=len(LGAs)
 
-# vowel= "aeiou"
-# for i in range(length):
-#     if LGAs[i][0].lower() in vowel:
-#          print(LGAs[i])
+# vowel= "AEIOUaeiou"
+# # for i in range(length):
+# #      if LGAs[i][0] in vowel:
+# #          print(LGAs[i])
 
 # for lg in LGAs:
 #      if lg[0] in vowel:
-#          print(lg)
-# #Assignment
+#           print(lg)
+# # #Assignment
 # #go to w3 schools, check everything on list, also, under loop, do the practice, read on functions and classes
 
 # # double(5)
@@ -47,8 +44,15 @@ LGAs= ["Ilorin west", "Ekiti", "Oke-ero", "Ifelodun", "Baruteen", "Asa", "Ilorin
 # # double
 # numbers=[1,2,3,4]
 # numbers.pop(1)
+
+
+#Assignment 1
+#Add LG to the end of each variable in the list "LGAS"
+#solution
 #create an emptylist
 modify_LGAS= []
+#loop through the list using a for loop
 for i in LGAs:
+#append "LG" to the new list using the formular below
     modify_LGAS.append(i + " LG")
 print(modify_LGAS)
